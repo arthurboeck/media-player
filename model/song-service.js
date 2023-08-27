f143d3459b34518595ad63e514829f08d19cc18b
guilherme-cmd <54598542+guilherme-cmd@users.noreply.github.com>
2023-08-27 20:33:51 -0300

    first steps with object song and yours routes

---
+++ model/song-service.js
@@ -0,0 +1,14 @@
+
+class Song {
+    constructor(id, nome, artista, album, tamanho, duracao, rota) {
+        this.id = id;
+        this.nome = nome;
+        this.artista = artista;
+        this.album = album;
+        this.tamanho = tamanho;
+        this.duracao = duracao;
+        this.rota = rota;
+    }
+}
+
+module.exports = Song;
