6b3941d5089172a710961d012cdf3426096c3101
Arthur Boeck <arthurguterres@outlook.com>
2023-08-28 22:05:45 -0300

    add controller playlist

--- controller/playlist-controller.js
+++ controller/playlist-controller.js
@@ -1,97 +1,85 @@
-// const playlistService = require('../model/playlist-service');
+const playlistService = require('../model/playlist-service.js');
 
-// module.exports = function (app) {
+module.exports = function (router) {
+    router.get('/api/v1/playlists', (req, res) => {
+        const data = playlistService.findAllPlaylists();
 
-//     app.post(`/api/v1/data`, async (req, res) => {
-//         db.insert(playlist, (err, data) => {
-//             if (err) {
-//                 console.log(err);
-//                 res.status(500).send();
-//             }
-//             res.status(201).send();
-//         });
-    
-//         // res.status(201).send();
-//     });
-    
+        if (data) {
+            res.status(200).json(data);
+        } else {
+            res.status(404).send();
+        }
+    });
 
-//     app.get('/api/v1/playlists', (req, res) => {
-//         playlistService.listAllPlaylists().then((playlists) => {
-//             res.status(200).json(playlists);
-//         }).catch((err) => {
-//             res.status(500).send();
-//         });
-//     });
+    router.get('/api/v1/playlists/:id/detail', (req, res) => {
+        const id = parseInt(req.params.id);
+        const data = playlistService.findPlaylistById(id);
 
-//     app.post('/api/v1/playlists', (req, res) => {
-//         const requestBody = req.body;
+        if (data) {
+            res.status(200).json(data);
+        } else {
+            res.status(404).send();
+        }
+    });
 
-//         const playlist = {
-//             nome: requestBody.nome,
-//             descricao: requestBody.descricao,
-//             musicas: songs = {
-//                 id: requestBody.id,
-//                 nome: requestBody.nome,
-//                 artista: requestBody.artista,
-//                 album: requestBody.album,
-//                 duracao: requestBody.duracao,
-//                 tamanho: requestBody.tamanho,
-//                 rota: requestBody.rota,
-//             }
-//         };
+    router.delete('/api/v1/playlists/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        playlistService.deletePlaylistById(id);
+        res.status(204).send();
+    });
 
-//         playlistService.createPlaylist(playlist).then(() => {
-//             res.status(201).send();
-//         }).catch((err) => {
-//             res.status(500).send();
-//         });
-//     });
+    router.post('/api/v1/playlists', (req, res) => {
+        const requestBody = req.body;
 
-// };  
+        const playlist = {
+            nome: requestBody.nome,
+            descricao: requestBody.descricao,
+        };
 
-// const playlist = {
-//     name: 'Minha playlist',
-//     description: 'Descrição da minha playlist',
-//     public: true,
-//     collaborative: false,
-//     songs: [
-//         {
-//             id: 1,
-//             name: 'Música 1',
-//             duration: 120
-//         },
-//         {
-//             id: 2,
-//             name: 'Música 2',
-//             duration: 120
-//         },
-//         {
-//             id: 3,
-//             name: 'Música 3',
-//             duration: 120
-//         }
-//     ]
-// };
-// const playlist2 = {
-//     name: 'Sua playlist',
-//     description: 'Descrição da minha playlist',
-//     public: false,
-//     collaborative: true,
-//     songs: [
-//         {
-//             id: 1,
-//             name: 'Música 1',
-//             duration: 120
-//         },
-//         {
-//             id: 2,
-//             name: 'Música 2',
-//             duration: 120
-//         },
-//         {
-//             id: 3,
-//             name: 'Música 3',
-//             duration: 120
-//         }
-//     ]
-// };
+        playlistService.createPlaylist(playlist)
+        res.status(201).send();
+    });
+
+    router.put('/api/v1/playlists/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const idx = playlistService.findPlaylistIndexById(id);
+
+        if (idx === -1) {
+            res.status(404).send();
+        } else {
+            const updatedPlaylist = {
+                id: id,
+                nome: req.body.nome,
+                descricao: req.body.descricao
+            };
+
+            playlistService.updatePlaylistByIndex(idx, updatedPlaylist)
+            res.status(200).send();
+        }
+    });
+
+    router.delete('/api/v1/playlists/:id/song/:songId', (req, res) => {
+        const id = parseInt(req.params.id);
+        const songId = parseInt(req.params.songId);
+
+        playlistService.removeSongFromPlaylistById(id, songId);
+        res.status(204).send();
+    });
+
+    router.post('/api/v1/playlists/:id/song', (req, res) => {
+        const id = parseInt(req.params.id);
+        const requestBody = req.body;
+
+        const song = {
+            nome: requestBody.nome,
+            artista: requestBody.artista,
+            album: requestBody.album,
+            duracao: requestBody.duracao,
+            tamanho: requestBody.tamanho,
+            rota: requestBody.rota
+        };
+
+        playlistService.addSongToPlaylist(id, song);
+        res.status(201).send();
+    });
+};
--- model/playlist-service.js
+++ model/playlist-service.js
@@ -1,13 +1,61 @@
-// function listAllPlaylists() {
-//     return db.find({}).exec();
-// };
+const Song = require('./song-service');
 
-// async function createPlaylist(playlist) {
-//     await db.insert(playlist);
-// };
+class Playlist {
+    constructor(id, nome, descricao, musicas) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.musicas = musicas;
+    }
+}
 
-// function listPlaylistByFilter(filter) {
-//     return db.find(filter).exec();
-// };
+const playListData = {
+    data: [
+        new Playlist(1, 'Minha playlist', 'Descrição da minha playlist', [new Song(1, 'Música 1', 'Artista 1', 'Album 1', 120, 120, 'public/songs/musica1.mp3'), new Song(2, 'Música 2', 'Artista 2', 'Album 2', 120, 120, 'public/songs/musica1.mp3')]),
+        new Playlist(2, 'Sua playlist', 'Descrição da sua playlist', [new Song(1, 'Música 1', 'Artista 1', 'Album 1', 120, 120, 'public/songs/musica1.mp3')]),
+        new Playlist(3, 'Minha nova playlist', 'Descrição da minha nova playlist', [new Song(2, 'Música 2', 'Artista 2', 'Album 2', 120, 120, 'public/songs/musica2.mp3'), new Song(3, 'Música 3', 'Artista 3', 'Album 3', 120, 120, 'public/songs/musica3.mp3')]),
+    ]
+};
 
-// module.exports = { listAllPlaylists, createPlaylist, listPlaylistByFilter };
+function findAllPlaylists() {
+    return playListData.data;
+}
+
+function findPlaylistById(id) {
+    return playListData.data.find(p => p.id === id);
+}
+function findPlaylistIndexById(id) {
+    return playListData.data.findIndex(p => p.id === id);
+}
+
+function createPlaylist(playlist) {
+    const id = playListData.data.length + 1;
+    const newPlaylist = new Playlist(id, playlist.nome, playlist.descricao, []);
+    playListData.data.push(newPlaylist);
+};
+
+function updatePlaylistByIndex(index, newData) {
+    const playlist = findPlaylistById(newData.id);
+    const playlistAtualizada = new Playlist(playlist.id, newData.nome, newData.descricao, playlist.musicas);
+    playListData.data[index] = playlistAtualizada;
+};
+
+function deletePlaylistById(id) {
+    const index = playListData.data.findIndex(p => p.id === id);
+    playListData.data.splice(index, 1);
+}
+
+function addSongToPlaylist(idPlaylist, song) {
+    const playlist = findPlaylistById(idPlaylist);
+    const idSong = playlist.musicas.length + 1;
+    const newSong = new Song(idSong, song.nome, song.artista, song.album, song.duracao, song.duracao, song.rota);
+    playlist.musicas.push(newSong);
+}
+
+function removeSongFromPlaylistById(id, songId) {
+    const playlist = findPlaylistById(id);
+    const index = playlist.musicas.findIndex(s => s.id === songId);
+    playlist.musicas.splice(index, 1);
+}
+
+module.exports = { Playlist, findAllPlaylists, findPlaylistById, createPlaylist, updatePlaylistByIndex, deletePlaylistById, addSongToPlaylist, removeSongFromPlaylistById, findPlaylistIndexById };
